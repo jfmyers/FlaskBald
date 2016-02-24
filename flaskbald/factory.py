7fa36dcf5b242f7f359a59429dd6938cf69cffa8
Jim Myers <jfmyers01@gmail.com>
2016-02-24 17:29:38 +0000

    Add ability to deny access to certain hosts. Add handlers for before/after requests. For the 'after' request handler, commit the database session and close it out.

--- flaskbald/factory.py
+++ flaskbald/factory.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+#!/usr/bin/env python
+# encoding: utf-8
+from flask import Flask, render_template, request
 from db_ext import db
+from response import APINotFound, api_action
 from log import default_debug_log
 import os
 import jinja2
+
+
+ALLOWED_HOSTS = 'ALLOWED_HOSTS'
+ALL_HOSTS = '*'
 
 
 def load_config(app, config_file=None, env='development'):
@@ -10,6 +17,9 @@ def load_config(app, config_file=None, env='development'):
 		return app
 
 	app.config.from_pyfile(config_file)
+	if not app.config.get(ALLOWED_HOSTS):
+		app.config.update({ALLOWED_HOSTS: ALL_HOSTS})
+
 	return app
 
 	# var = 'APP_ENV'
@@ -67,33 +77,103 @@ def error_endpoints(app, custom_error_endpoints=False):
 	return app
 
 
+def before_handler(app, custom_handler, custom_handler_args, custom_handler_kargs):
+
+	if custom_handler:
+		@app.before_request
+		def before_request():
+			custom_handler(*custom_handler_args, **custom_handler_kargs)
+
+		return app
+
+	@app.before_request
+	def before_request():
+		origin_request = request.from_values()
+		if app.config[ALLOWED_HOSTS] != ALL_HOSTS and origin_request.host not in app.config[ALLOWED_HOSTS]:
+			if app.config.get('DEBUG', app.config.get('debug')):
+				return APINotFound(message="Invalid host: '{0}'".format(origin_request.host))
+			else:
+				return APINotFound()
+
+	return app
+
+
+def after_handler(app, custom_handler, custom_handler_args, custom_handler_kargs):
+
+	if custom_handler:
+		@app.after_request
+		def after_request(response):
+			custom_handler_kargs['response'] = response
+			return custom_handler(*custom_handler_args, **custom_handler_kargs)
+
+		return app
+
+	@app.after_request
+	def after_request(response):
+		# Commit the session
+		db.session.commit()
+
+		# Close the session
+		db.session.close()
+
+		# Return the response object
+		return response
+
+	return app
+
+
 def init_db(app):
 	db.init_app(app)
 	return app
 
 
 def create_app(config_file, blue_prints=[], custom_error_endpoints=False, 
-			   custom_template_path=None):
+			   custom_template_path=None, custom_before_handler=None,
+			   custom_before_handler_args=[], custom_before_handler_kargs={},
+			   custom_after_handler=None, custom_after_handler_args=[],
+			   custom_after_handler_kargs={}):
 
 	if config_file is None:
 		raise(Exception("Hey, 'config_files' cannot be 'None'!"))
 
-	return (
-		init_db(
-			error_endpoints(
-				register_blue_prints(
-					setup_debug_log(
-						setup_templates(
-							load_config(
-								Flask(__name__),
-								config_file
-							),
-							custom_template_path
-						)
-					),
-					blue_prints
-				),
-				custom_error_endpoints
-			)
-		)
-	)
+	app = Flask(__name__)
+	app = load_config(app, config_file)
+	app = setup_templates(app, custom_template_path)
+	app = setup_debug_log(app)
+	app = register_blue_prints(app, blue_prints)
+	app = error_endpoints(app, custom_error_endpoints)
+	app = before_handler(app, custom_before_handler, custom_before_handler_args, custom_before_handler_kargs)
+	app = after_handler(app, custom_after_handler, custom_after_handler_args, custom_after_handler_kargs)
+	app = init_db(app)
+
+	return app
+
+	# return (
+	# 	init_db(
+	# 		after_handler(
+	# 			before_handler(
+	# 				error_endpoints(
+	# 					register_blue_prints(
+	# 						setup_debug_log(
+	# 							setup_templates(
+	# 								load_config(
+	# 									Flask(__name__),
+	# 									config_file
+	# 								),
+	# 								custom_template_path
+	# 							)
+	# 						),
+	# 						blue_prints
+	# 					),
+	# 					custom_error_endpoints
+	# 				),
+	# 				custom_before_handler,
+	# 				custom_before_handler_args,
+	# 				custom_before_handler_kargs
+	# 			),
+	# 			custom_after_handler,
+	# 			custom_after_handler_args,
+	# 			custom_after_handler_kargs
+	# 		)
+	# 	)
+	# )
