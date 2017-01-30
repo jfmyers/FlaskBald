a824d5c25958754d09cdcd6860905c46b93945a7
Jim Myers <jfmyers01@gmail.com>
2017-01-30 09:31:53 -0500

    Update Flaskbald auth to work with Auth0.

--- flaskbald/__init__.py
+++ flaskbald/__init__.py
@@ -1,8 +1,14 @@
 __version__ = '1.0.0'
 
-# import console
-# import log
-# import model
-# import text
-
-# from templates import *
+import auth
+import celery_ext
+import console
+import db_ext
+import factory
+import log
+import model
+import password
+import response
+import template
+import text
+import validate
--- flaskbald/auth.py
+++ flaskbald/auth.py
@@ -8,55 +8,54 @@ import json
 
 
 def create_jwt(secret, payload={}, exp=date.utcnow() + timedelta(days=7),
-               iat=date.utcnow(), handle='mrbayes', algorithm='HS256'):
+               iat=date.utcnow(), algorithm='HS256'):
     payload.update({
         'exp': exp,
-        'iat': iat,
-        'handle': handle
+        'iat': iat
     })
     return jwt.encode(payload, secret, algorithm=algorithm)
 
 
-def decode_jwt(token, secret, algorithm='HS256'):
-    return jwt.decode(token, secret, algorithm=algorithm)
+def decode_jwt(token, secret, audience=None, algorithm='HS256'):
+    if not audience:
+        jwt.decode(token, secret, algorithm=algorithm)
+    return jwt.decode(token, secret, audience=audience, algorithm=algorithm)
 
 
 def _auth_error():
     raise APIUnauthorized("User authentication is required to access this resource.")
 
 
-def get_jwt(jwt_cookie_key='jwt'):
-    secret = current_app.config.get('JWT_AUTH_SECRET')
+def get_jwt_claims(jwt_key='Authorization'):
+    secret = current_app.config.get('JWT_CLIENT_SECRET')
+    audience = current_app.config.get('JWT_AUDIENCE')
+
     if not secret:
         raise APIError("JWT secret not specified. Contact hello@mylestoned.com for assistance.")
 
-    jwtoken = request.cookies.get(jwt_cookie_key)
+    jwtoken = request.headers.get(jwt_key, request.cookies.get(jwt_key))
     if not jwtoken:
         _auth_error()
 
     try:
-        decoded = decode_jwt(jwtoken, secret)
+        jwt_claims = decode_jwt(jwtoken, secret, audience=audience)
     except (jwt.ExpiredSignatureError, jwt.DecodeError):
         _auth_error()
 
-    user_id = decoded.get('user_id')
-    if not user_id:
+    sub = jwt_claims.get('sub')
+    if not sub:
         _auth_error()
 
-    return decoded
+    return jwt_claims
 
 
-def user_required(orig_func=None, jwt_cookie_key='jwt'):
+def user_required(orig_func=None, jwt_key='jwt'):
 
     def requirement(orig_func):
         '''Requirement decorator.'''
         @wraps(orig_func)
         def replacement(*pargs, **kargs):
-            decoded = get_jwt(jwt_cookie_key=jwt_cookie_key)
-            user_id = decoded.get('user_id')
-            if not user_id:
-                _auth_error()
-
+            get_jwt_claims(jwt_key=jwt_key)
             return orig_func(*pargs, **kargs)
 
         return replacement
@@ -67,13 +66,11 @@ def user_required(orig_func=None, jwt_cookie_key='jwt'):
         return requirement(orig_func)
 
 
-def get_current_user(jwt_cookie_key='jwt'):
-    '''Get the current logged-in user, if user is logged-in'''
+def get_auth_id(jwt_key='jwt'):
+    '''Get the current logged-in users auth id'''
     try:
-        decoded = get_jwt(jwt_cookie_key=jwt_cookie_key)
+        jwt_claims = get_jwt_claims(jwt_key=jwt_key)
     except (APIError, APIUnauthorized):
-        decoded = {}
+        return None
 
-    return {
-        'id': decoded.get('user_id')
-    }
+    return jwt_claims.get('sub')
